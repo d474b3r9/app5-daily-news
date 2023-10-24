71557a630d743fd7c28e4a4c21ce3df99722ca6c
jonathan cattelain <jonathan.cattelain@gmail.com>
2023-10-24 17:40:25 +0200

    finished today

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ import os
 load_dotenv()
 api_key = os.getenv('API_KEY')
 
+# make a request
+url = f"https://newsapi.org/v2/everything?" \
+      f"q=tesla&sortBy=publishedAt&apiKey={api_key}"
 
-url = "https://finance.yahoo.com"
+# Get the dict
 request = requests.get(url)
-content = request.text
-print(content)
+content = request.json()
 
-# URL=https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=
+# print titles
+for article in content["articles"]:
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
