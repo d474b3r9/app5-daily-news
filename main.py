7886c66ed783ebe21bd6cdc3a2ebbf6e26750b11
jonathan cattelain <jonathan.cattelain@gmail.com>
2023-10-26 10:32:41 +0200

    finished today

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import requests
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from send_email import send_email
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
+topic = "tesla"
 
 # make a request
-url = f"https://newsapi.org/v2/everything?" \
-      f"q=tesla&sortBy=publishedAt&apiKey={api_key}"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&" \
+      f"apiKey={api_key}&" \
+      "language=en"
 
-# Get the dict
+# Make the request
 request = requests.get(url)
+
+# Get the contents
 content = request.json()
 
 # print titles
+body = "test"
 for article in content["articles"]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = body + article["title"] + "\n" \
+               + article["description"] + "\n" \
+               + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
--- send_email.py
+++ send_email.py
@@ -1,16 +1,55 @@
-import smtplib, ssl
+import smtplib
+import ssl
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def send_email(message):
     host = "smtp.gmail.com"
-    port = 465
+    port = 465  # TLS
+    smtp_user = os.getenv('SMPT_USERS')
+    smtp_key = os.getenv('SMTP_KEY')
 
-    username = "app8flask@gmail.com"
-    password = "YOUR_GMAIL_APP_PASSWORD"
-
-    receiver = "app8flask@gmail.com"
+    receiver = smtp_user
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
+        server.login(smtp_user, smtp_key)
+        server.sendmail(smtp_user, receiver, message)
+
+
+smtp_server = "smtp.gmail.com"
+port = 587  # For starttls
+smtp_user = os.getenv('SMPT_USERS')
+smtp_key = os.getenv('SMPT_KEY')
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def sendmailx(message):
+    message = MIMEMultipart()
+    message["To"] = smtp_user
+    message["From"] = smtp_user
+    message["Subject"] = 'Subject line here.'
+
+    title = '<b> Title line here. </b>'
+    message_text = MIMEText('''Message body goes here.''', 'html')
+    message.attach(message_text)
+
+    email = smtp_user
+    password = smtp_key
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo('Gmail')
+    server.starttls()
+    server.login(email, password)
+    fromaddr = 'From line here.'
+    toaddrs = 'Address you send to.'
+    server.sendmail(fromaddr, toaddrs, message.as_string())
+
+    server.quit()
